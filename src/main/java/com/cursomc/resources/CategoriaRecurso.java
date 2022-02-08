68209ce64cae2547cbfe65e49e4460f781833e08
Marcus Dimitri <marcusdimitri7@gmail.com>
2022-02-08 19:01:06 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/cursomc/resources/CategoriaRecurso.java
+++ src/main/java/com/cursomc/resources/CategoriaRecurso.java
@@ -3,14 +3,21 @@
  */
 package com.cursomc.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.dto.CategoriaDTO;
 import com.cursomc.services.CategoriaServico;
 
 /**
@@ -27,9 +34,31 @@ public class CategoriaRecurso {
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 
 	}
+
+	// Para Testes
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	/*
+	 * //@PreAuthorize("hasAnyRole('ADMIN')")
+	 * 
+	 * @RequestMapping(method=RequestMethod.POST) public ResponseEntity<Void>
+	 * insert( @RequestBody CategoriaDTO objDto) { //@Valid Categoria obj =
+	 * service.fromDTO(objDto); obj = service.insert(obj); URI uri =
+	 * ServletUriComponentsBuilder.fromCurrentRequest()
+	 * .path("/{id}").buildAndExpand(obj.getId()).toUri(); return
+	 * ResponseEntity.created(uri).build(); }
+	 */
+
+	// Codigos para fins de teste. Serve como base para o inicio de implementação.
 
 }
--- src/main/java/com/cursomc/services/CategoriaServico.java
+++ src/main/java/com/cursomc/services/CategoriaServico.java
@@ -3,15 +3,20 @@
  */
 package com.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
-
-import com.cursomc.domain.Categoria;
-import com.cursomc.repositories.CategoriaRepositorio;
+import com.cursomc.services.exceptions.DataIntegrityException;
 import com.cursomc.services.exceptions.ObjectNotFoundException;
+import com.cursomc.repositories.CategoriaRepositorio;
+import com.cursomc.domain.Categoria;
+import com.cursomc.dto.CategoriaDTO;
 
 /**
  * @author Marcus Dimitri
@@ -23,17 +28,61 @@ public class CategoriaServico   {
 	@Autowired
 	private CategoriaRepositorio repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id); 
 		return obj.orElseThrow(() -> new ObjectNotFoundException( 
 		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+	
+	
+	//Para Testes
+		public Categoria insert(Categoria obj) {
+			obj.setId(null);
+			return repo.save(obj);
+		}
+	
+	
+		/*
+		 * public Categoria insert(Categoria obj) { obj.setId(null); return
+		 * repo.save(obj); }
+		 */
+	
+	public Categoria update(Categoria obj) {
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 
 
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	}
 	
 	
-	
-	
+	//Codigos para fins de Teste.Serve como referencia no inicio do desenvolvimento.
 	
 
 }
