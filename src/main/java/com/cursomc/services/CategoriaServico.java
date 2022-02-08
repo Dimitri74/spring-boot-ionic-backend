ed0a4e6972ec635cde9e4b4df9b96a00aa8d3ebf
Marcus Dimitri <marcusdimitri7@gmail.com>
2022-02-08 19:31:04 -0300

    Atualizando Categoria PUT , outros ajustes

--- src/main/java/com/cursomc/services/CategoriaServico.java
+++ src/main/java/com/cursomc/services/CategoriaServico.java
@@ -23,66 +23,57 @@ import com.cursomc.dto.CategoriaDTO;
  *
  */
 @Service
-public class CategoriaServico   {
+public class CategoriaServico {
 
 	@Autowired
 	private CategoriaRepositorio repo;
 
 	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id); 
-		return obj.orElseThrow(() -> new ObjectNotFoundException( 
-		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
-	
-	//Para Testes
-		public Categoria insert(Categoria obj) {
-			obj.setId(null);
-			return repo.save(obj);
-		}
-	
-	
-		/*
-		 * public Categoria insert(Categoria obj) { obj.setId(null); return
-		 * repo.save(obj); }
-		 */
-	
+
+	// Para Testes
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	// Para Testes
 	public Categoria update(Categoria obj) {
-		Categoria newObj = find(obj.getId());
-		updateData(newObj, obj);
-		return repo.save(newObj);
+		find(obj.getId());
+
+		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
 			repo.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
 	}
-	
+
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-
 
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
 	}
-	
+
 	private void updateData(Categoria newObj, Categoria obj) {
 		newObj.setNome(obj.getNome());
 	}
-	
-	
-	//Codigos para fins de Teste.Serve como referencia no inicio do desenvolvimento.
-	
+
+	// Codigos para fins de Teste.Serve como referencia no inicio do
+	// desenvolvimento.
 
 }
